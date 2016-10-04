412caea4b806fd3569a89de1f5c14b5c5a03f4ef
drabadan <cfymjr@gmail.com>
2016-10-04 16:36:13 +0300

    TestApplication Commit

---
+++ DrabadanCoreLib_WF_TestApp/DrabCL_AppForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DrabadanCoreLib_WF_TestApp
+{
+    partial class DrabCL_App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Test_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 178);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(455, 224);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Test_button
+            // 
+            this.Test_button.Location = new System.Drawing.Point(12, 12);
+            this.Test_button.Name = "Test_button";
+            this.Test_button.Size = new System.Drawing.Size(192, 43);
+            this.Test_button.TabIndex = 1;
+            this.Test_button.Text = "Test";
+            this.Test_button.UseVisualStyleBackColor = true;
+            this.Test_button.Click += new System.EventHandler(this.Test_button_Click);
+            // 
+            // DrabCL_App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 402);
+            this.Controls.Add(this.Test_button);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "DrabCL_App";
+            this.Text = "Drabadan Core Library WinForms Test App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Test_button;
+    }
+}
+
---
+++ DrabadanCoreLib_WF_TestApp/DrabCL_AppForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StealthAPI;
+using DrabadanCoreLib.Gumps;
+
+namespace DrabadanCoreLib_WF_TestApp
+{
+    public partial class DrabCL_App : Form
+    {
+        public DrabCL_App()
+        {
+            InitializeComponent();
+        }
+
+        private void SendConsoleMessage(string message)
+        {
+            this?.Invoke((MethodInvoker)delegate
+            {
+                richTextBox1.SelectionStart = 0;
+                richTextBox1.SelectionLength = 0;
+                richTextBox1.SelectedText = DateTime.Now + ": " + message + Environment.NewLine;
+            });
+        }
+
+        private async void Test_button_Click(object sender, EventArgs e)
+        {
+            GetGumpInfoTest();
+        }
+
+        private static Stealth _stealth = Stealth.Client;
+
+        private void GetGumpInfoTest()
+        {
+            _stealth.IncomingGump += (sender, e) =>
+             {
+                 SendConsoleMessage($"{e.GumpId}");
+                 Gump g = Gump.GetGump(e.GumpId);
+                 SendConsoleMessage($"{g.Buttons.Count}");
+                 g.TextEdits.ForEach(ed => ed.Text = "Hello");
+                 g.Buttons.First().Click();
+             };
+            
+        }
+    }
+}
