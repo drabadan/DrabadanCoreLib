9e8b278849298515e8ca879b29733e1a2107df95
Drabadan <cfymjr@gmail.com>
2016-10-15 10:28:09 +0300

    Recommiting last commit

---
+++ PreLaunchHandlings/CharSelectorHandler_Dialog.cs
@@ -0,0 +1,42 @@
+﻿using DrabadanCoreLib.Common.WinapiHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrabadanCoreLib.PreLaunchHandlings
+{
+    public class CharSelectorHandler_Dialog
+    {
+        public CharSelectorHandler_Dialog() { }
+
+        private static async Task<string> TryToFindCharTitleAsync()
+        {
+            var process = await GetWindowsTitlesAndHWNDS.SearchForProcessByNameAsync("Stealth");
+            if (process == null)
+                return "";
+
+            var listOfTitles = await GetWindowsTitlesAndHWNDS.GetChildWindowsTitlesAsync(process);
+            if(listOfTitles.Count > 0)
+            {
+                int idx = listOfTitles.IndexOf("Main");
+                if (idx > 0)
+                    return listOfTitles[idx + 1];
+            }
+
+            return "";
+        } 
+
+        public async Task<DialogResult> CharacterSelectorResult()
+        {
+            var charTitle = await TryToFindCharTitleAsync();
+            if (charTitle == "")
+                return DialogResult.Abort;
+
+            return MessageBox.Show($"Connecting to: {charTitle}", "Character informer", MessageBoxButtons.YesNo);
+        }
+
+    }
+}
